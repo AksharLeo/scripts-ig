09b8fa1c41c48add462450fef24338d048888bf8
AksharLeo <aksharthirunahari@gmail.com>
2021-03-04 11:10:07 +0530

    Update clear shit.bat

--- clear shit.bat
+++ clear shit.bat
@@ -1,22 +1,4 @@
 @echo off
-
-call :isAdmin
-
-if %errorlevel% == 0 (
-    goto :run
-) else (
-    echo Requesting administrative privileges...
-    goto :UACPrompt
-)
-
-exit /b
-
-:isAdmin
-    fsutil dirty query %systemdrive% >nul
-exit /b
-
-:run
-
 del /s /f /q %windir%\temp\*.*    
 rd /s /q %windir%\temp    
 md %windir%\temp    
@@ -51,12 +33,4 @@ del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Recent Items*.* /Q pau
 
 ipconfig/flushdns
 
-exit /b
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-exit /B
+cmd/k
