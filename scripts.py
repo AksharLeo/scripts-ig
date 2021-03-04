9ce04cc2761071b6c8e6f944b2051946783fcd3d
AksharLeo <aksharthirunahari@gmail.com>
2021-03-04 17:18:28 +0530

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -26,6 +26,8 @@ new_install = '''
 
             sudo apt install fish piper shotwell flameshot alacritty gnome-tweaks gnome-shell-extensions discord gparted mpv -y
             sudo apt install brave-browser -y
+            sudo apt-get install libcurl4-openssl-dev
+            sudo apt-get install pkg-config
             sudo apt-get update && sudo apt-get install spotify-client -y
             sudo apt-get install gtk2-engines-murrine gtk2-engines-pixbuf git
 
